25358207a1552c45f192cbcf624864a74b51dd73
DaniloSVJ <danilocotatoceu@gmail.com>
2022-03-25 15:30:08 -0300

    add

--- src/App.js
+++ src/App.js
@@ -168,7 +168,7 @@ function App() {
       dia = data.getDate().toString().padStart(2, '0'),
       mes = (data.getMonth() + 1).toString().padStart(2, '0'),
       ano = data.getFullYear();
-      console.log()
+      console.log(`o mês é: ${mes}`)
     return `${dia}/${mes}/${ano}`;
   }
   return (
